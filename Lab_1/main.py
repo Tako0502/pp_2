63580a299b4e16eb6fce85a80684484cebe455e8
ITalant7 <64712032+Tako0502@users.noreply.github.com>
2024-01-27 16:22:46 +0600

    1

---
+++ Lab_1/main.py
@@ -0,0 +1,184 @@
+# #-------------task=1------------------------
+print("Hello, world!")
+# #---------------------task------------------------
+if 5 > 2:
+  print("Five is greater than two!")
+# #---------------------task------------------------
+
+  if 5 > 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+# #---------------------task------------------------
+        '''''''''''
+        This 
+        is 
+        a 
+        comment
+        '''''''''''
+# #---------------------task------------------------
+        x =4
+        y="Hello world"
+        print(x)
+        print(y)
+# #---------------------task------------------------
+        x=4 
+        x="Hello world"
+        print(x)
+# #---------------------task------------------------
+a = str(3)    
+b= int(3)    
+c = float(3)  
+# #---------------------task------------------------
+a = 5
+b = "John"
+print(type(a))
+print(type(b))
+# #---------------------task------------------------
+a = 4
+A = "Sally"
+print(A)
+print(a)
+# #---------------------task------------------------
+a, b, c = "bir", "eki", "ush"
+print(a)
+print(b)
+print(c)
+# #---------------------task------------------------
+a1 = a2 = a3 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+print(a1)
+print(a2)
+print(a3)
+# #---------------------task------------------------
+colors = ["white", "black", "red"]
+x, y, z = colors
+print(x)
+print(y)
+print(z)
+# #---------------------task------------------------
+x = "Python is awesome"
+print(x)
+# #---------------------task------------------------
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+# #---------------------task------------------------
+x = "Hello World"	
+print(x) 	
+x = 20	 	
+print(x) 	
+x = 20.5	 	
+print(x) 	
+x = 1j	 	
+print(x) 	
+x = ["apple", "banana", "cherry"]	 	
+print(x) 	
+x = ("apple", "banana", "cherry")	 	
+print(x) 	
+x = range(6)	 	
+print(x) 	
+x = {"name" : "John", "age" : 36}	 	
+print(x) 	
+x = {"apple", "banana", "cherry"}	 	
+print(x) 	
+x = frozenset({"apple", "banana", "cherry"})	 	
+print(x) 	
+x = True	   	
+print(x) 	
+x = b"Hello"	 	
+print(x) 	
+x = bytearray(5)	 	
+print(x) 	
+x = memoryview(bytes(5))	 	
+print(x) 	
+x = None
+print(x) 	
+# #---------------------task------------------------
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+# #---------------------task------------------------
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+# #---------------------task------------------------
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+# #---------------------task------------------------
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(x)
+print(y)
+print(z)
+x = 1    
+y = 2.8  
+z = 1j  
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+# #---------------------task------------------------
+
+import random
+import string
+random_letter = random.choice(string.ascii_letters)
+print(random_letter)
+# #---------------------task------------------------
+x = float(1)     
+y = int(2.8)  
+z = str(3)  
+# #---------------------task------------------------
+for x in "banana":
+  print(x) 
+# #---------------------task------------------------
+  a = "Hello, World!"
+print(len(a))
+# #---------------------task------------------------
+txt = "python is fast language"
+print("fast" in txt)
+print(txt[0:7]+txt[10:15])
+print(txt[:5])
+print(txt[2:])
+print(txt[-5:-2])
+# #---------------------task------------------------
+university = "  Kbtu  "
+print(university.upper())
+print(university.lower())
+print(university.strip())  
+print(university.replace("Kbtu", "The best uni"))
+# #---------------------task------------------------
+Unuversity = "Kazakh British Technical University"
+Uni = Unuversity.split(" ")
+for i in Uni:
+      print(i)
+# #---------------------task------------------------
+a="hello"
+b="world"
+c=a+" "+b
+print(c)
+# #---------------------task------------------------
